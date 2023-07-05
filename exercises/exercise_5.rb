4e9c21732ace351a224bafe6f748cfdd214703dc
Denny Ng <Dennyng1989@hotmail.com>
2023-07-04 22:27:14 -0700

    Commit ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "The total company revenue is: $#{Store.sum(:annual_revenue)}."
+puts "The average store revenue is $#{Store.average(:annual_revenue)}."
+puts "There are #{Store.where(annual_revenue: 1000000..Float::INFINITY).count} stores generating more than $1,000,000 per year."
